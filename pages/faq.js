0cd9cbcdd0f38af43dc80cbc391863c5772e586d
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2023-01-20 22:53:16 -0300

    creating link component

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,12 @@
+import Link from '../src/components/Link'
 
 export default function FAQPage () {
     return(
         <div>
             <h1>Página de perguntas</h1>
+            <Link href="/">
+              Ir para a Home
+            </Link>
         </div>
 
     )
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import  Link  from '../src/components/Link'
 
 export default function HomePage () {
     return(
         <div>
             <h1>Alura Cases</h1>
+           <Link href="/faq">
+            Ir para o faq
+           </Link>
         </div>
     )
 }
